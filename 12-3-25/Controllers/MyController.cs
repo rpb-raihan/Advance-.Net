7b6e6c5db14ac6f6ca9b13f44f3faa2659aa9b10
USER <USER@DESKTOP-PG6CR1V>
2025-03-17 20:07:40 +0600

    Add project files.

---
+++ 12-3-25/Controllers/MyController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _12_3_25.Controllers
+{
+    public class MyController : Controller
+    {
+        // GET: My
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+        public ActionResult Education()
+        {
+            return View();
+        }
+
+        public ActionResult Project()
+        {
+            return View();
+        }
+
+        public ActionResult Referance()
+        {
+            return View();
+        }
+
+    }
+}
